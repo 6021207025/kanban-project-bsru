c0feb5ad6db6f0a560444d104288ac26f2fab06c
Nueng <6021207025@bsru.ac.th>
2020-12-03 14:10:25 +0700

    Alert

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,42 @@
 <template>
   <div id="app">
-    <Kanban/>
+    <Kanban :data="data"/>
   </div>
 </template>
 
 <script>
 import Kanban from "./components/Kanban"
-
 export default {
   name: 'App',
   components: {
     Kanban
+  },
+  data(){
+    return {
+      data:[
+        {
+          name:"todo",
+          color:"#F5B271",
+          tasks:[]
+        },
+         {
+          name:"doing",
+          color:"#BEA771",
+          tasks:[]
+        },
+         {
+          name:"done",
+          color:"#1B796F",
+          tasks:[]
+        }
+      ]
+    }
   }
 }
 </script>
 
 <style>
-#app {
+  #app {
   height: 100vh;
-}
+  }
 </style>
